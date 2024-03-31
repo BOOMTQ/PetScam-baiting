1cde696e205b06d406394be086f9e0256665a8e6
BOOMTQ <if20281@bristol.ac.uk>
2024-03-31 02:33:00 +0100

    Add the time zone judgement to fit the BST time start from March 31st at 1am

--- crawler/email_crawl.py
+++ crawler/email_crawl.py
@@ -9,20 +9,32 @@ from archiver import archive
 from secret import API_KEY, API_BASE_URL
 
 from secret import CRAWLER_PROG_DIR, ADDR_SOL_PATH, MAIL_HANDLED_DIR, MAIL_SAVE_DIR
+import pytz
 
 logs_url = f'{API_BASE_URL}/events'
+gmt_zone = pytz.timezone('GMT')
+bst_zone = pytz.timezone('Europe/London')
 
-# Get the current UTC date and time
+# gmt and utc are the same in winter
 now_utc = datetime.utcnow()
+now_gmt = gmt_zone.localize(now_utc)
 
-# Set the time to midnight (00:00:00) to get the start of today in UTC
-today_utc = datetime.combine(now_utc.date(), datetime.min.time())
+is_dst_now = now_gmt.astimezone(bst_zone).dst() != timedelta(0)
 
-# Subtract one day to get the start of yesterday in UTC
-day_before_today_utc = today_utc - timedelta(days=1)
+# check if now is bst_zone(3.31-10.27)
+if is_dst_now:
+    # transform gmt to bst
+    now_bst = now_gmt.astimezone(bst_zone)
+    today_bst = now_bst.replace(hour=0, minute=0, second=0, microsecond=0)
+    day_before_today_bst = today_bst - timedelta(days=1)
+    # RFC 2822 format with the BST offset
+    one_day_ago = day_before_today_bst.strftime('%a, %d %b %Y %H:%M:%S +0100')
+else:
+    today_gmt = now_gmt.replace(hour=0, minute=0, second=0, microsecond=0)
+    day_before_today_gmt = today_gmt - timedelta(days=1)
+    # RFC 2822 format with the GMT offset
+    one_day_ago = day_before_today_gmt.strftime('%a, %d %b %Y %H:%M:%S +0000')
 
-# Format the date as a string in RFC 2822 format with the UTC offset, include emails in day1, day2
-one_day_ago = day_before_today_utc.strftime('%a, %d %b %Y %H:%M:%S +0000')
 
 downloaded_emails = [f for f in listdir(MAIL_SAVE_DIR) if isfile(join(MAIL_SAVE_DIR, f))]
 handled_emails = [f for f in listdir(MAIL_HANDLED_DIR) if isfile(join(MAIL_HANDLED_DIR, f))]
